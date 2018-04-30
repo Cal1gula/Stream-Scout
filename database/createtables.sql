04a69e990aa75163e9d79bc0d0cdf6f83329918b
Cal1gula <jehorwath@gmail.com>
2018-04-29 20:35:52 -0400

    Create createtables.sql

---
+++ database/createtables.sql
@@ -0,0 +1,21 @@
+create table Users (
+id integer primary key,
+login varchar(25) not null,
+display_name varchar(25) not null,
+broadcaster_type varchar(50) not null default '',
+description varchar(300) not null default '',
+profile_image_url varchar(300) not null default '',
+offline_image_url varchar(300) not null default '',
+view_count bigint
+);
+
+create table Games (
+id int primary key,
+name varchar(300) not null default '',
+box_art_url varchar(300) not null default ''
+);
+
+create table UsersFollows (
+from_id integer references Users (id),
+to_id integer references Users (id),
+followed_at	datetime not null
